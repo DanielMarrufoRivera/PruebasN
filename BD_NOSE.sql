f43aa6fe4092866f2dfa4fdfd91c032fcb8d2da7
TaniaEsparza <31520735+TaniaEsparza@users.noreply.github.com>
2017-12-02 14:16:18 -0600

    _Base de datos actualizads

---
+++ BD_NOSE.sql
@@ -0,0 +1,30 @@
+CREATE TABLE Resultados (
+  idResultados INTEGER NOT NULL AUTO_INCREMENT,
+  Impuestos FLOAT NULL,
+  Ganancias FLOAT NULL,
+  Perdidas FLOAT NULL,
+  Utilidad_Bruta FLOAT NULL,
+  Fecha DATE NULL UNIQUE,
+  PRIMARY KEY(idResultados)
+);
+
+CREATE TABLE Recibo_Factura (
+  idRecibo_Factura INTEGER NOT NULL AUTO_INCREMENT,
+  Folio INTEGER NOT NULL,
+  Importe FLOAT NULL,
+  Fecha DATE NULL ,
+  Receptor_Nombre VARCHAR (100) NULL,
+  Emisor_Nombre VARCHAR (100) NULL,
+  RFC_Receptor VARCHAR (100) NULL,
+  RFC_Emisor VARCHAR (100) NULL,
+  Tipo VARCHAR (100) NOT NULL,
+  Impuesto FLOAT NULL,
+  Metodo_Pago VARCHAR (100) NOT NULL,
+  PRIMARY KEY(idRecibo_Factura)
+);
+
+CREATE TABLE Usuario (
+  idUsuario INTEGER NOT NULL ,
+  RFC VARCHAR (100) NOT NULL,
+  PRIMARY KEY(idUsuario)
+);
